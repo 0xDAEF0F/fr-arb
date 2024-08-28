72862da0250e85611cf47ee6565dc72d8d0cf69a
daemon <0xDAEF0F@proton.me>
2024-08-28 17:28:55 -0600

    cleaned up (now gets all tokents directly through hyperliquid)

--- src/hyperliquid/funding.rs
+++ src/hyperliquid/funding.rs
@@ -15,14 +15,14 @@ struct Universe {
 struct Token {
     sz_decimals: u8,
     name: String,
-    max_leverage: u32,
+    max_leverage: u8,
     only_isolated: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct FundingData {
-    funding: Option<String>,
+    funding: String,
     open_interest: String,
     prev_day_px: String,
     day_ntl_vlm: String,
@@ -33,7 +33,16 @@ struct FundingData {
     impact_pxs: Option<Vec<String>>,
 }
 
-pub async fn idk(info_client: &InfoClient) {
+#[derive(Debug)]
+pub struct HyperliquidToken {
+    pub name: String,
+    pub max_leverage: u8,
+    pub sz_decimals: u8,
+    pub funding_rate: f64,
+    pub open_interest: f64,
+}
+
+pub async fn retrieve_hl_tokens(info_client: &InfoClient) -> Vec<HyperliquidToken> {
     let data = json!({
         "type": "metaAndAssetCtxs"
     });
@@ -48,9 +57,25 @@ pub async fn idk(info_client: &InfoClient) {
     let tokens = ds.0.universe;
     let fr = ds.1;
 
+    let mut hyperliquid_tokens = vec![];
+
     for (token, funding_data) in tokens.into_iter().zip(fr.into_iter()) {
-        println!("token: {:#?} --- funding_data: {:#?}", token, funding_data);
+        let funding_rate: f64 = funding_data.funding.parse().unwrap();
+        let open_interest: f64 = funding_data.open_interest.parse().unwrap();
+        let open_interest: f64 = funding_data.oracle_px.parse::<f64>().unwrap() * open_interest;
+
+        let hyperliquid_token = HyperliquidToken {
+            name: token.name,
+            max_leverage: token.max_leverage,
+            sz_decimals: token.sz_decimals,
+            funding_rate,
+            open_interest,
+        };
+
+        hyperliquid_tokens.push(hyperliquid_token);
     }
+
+    hyperliquid_tokens
 }
 
 #[cfg(test)]
@@ -58,8 +83,29 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_idk() {
+    async fn get_all_hl_tokens() {
         let info_client = InfoClient::new(None, None).await.unwrap();
-        idk(&info_client).await;
+
+        let tokens = retrieve_hl_tokens(&info_client).await;
+
+        println!("{:#?}", tokens);
+    }
+
+    #[tokio::test]
+    async fn get_top_three_hl_fr() {
+        let info_client = InfoClient::new(None, None).await.unwrap();
+
+        let mut tokens = retrieve_hl_tokens(&info_client).await;
+
+        tokens.sort_by(|a, b| {
+            b.funding_rate
+                .abs()
+                .partial_cmp(&a.funding_rate.abs())
+                .unwrap()
+        });
+
+        let top_three: Vec<_> = tokens.into_iter().take(3).collect();
+
+        println!("{:#?}", top_three);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,14 @@
 mod hyperliquid;
 
 use anyhow::Result;
-use chrono::{Duration, Utc};
 use hyperliquid_rust_sdk::InfoClient;
-use log::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     env_logger::init();
 
-    let info_client = InfoClient::new(None, None).await.unwrap();
-
-    // let fh = funding_history_example(&info_client).await;
-
-    // println!("{:#?}", fh);
-
-    let tokens = get_tokens();
-
-    let token_plus_fr = retrieve_funding_rates_from_tokens(tokens.unwrap(), &info_client).await?;
-
-    println!("{:#?}", token_plus_fr);
-
-    // println!("{:#?}", tokens);
+    let _info_client = InfoClient::new(None, None).await.unwrap();
 
     Ok(())
-}
-
-fn get_tokens() -> Result<Vec<String>> {
-    let contents = std::fs::read_to_string("data/tokens.txt")?;
-
-    let tokens: Vec<String> = contents.split(',').map(String::from).collect();
-
-    Ok(tokens)
-}
-
-async fn retrieve_funding_rates_from_tokens(
-    tokens: Vec<String>,
-    info_client: &InfoClient,
-) -> Result<Vec<(String, f64)>> {
-    // Token - Funding Rate
-    let mut tokens_with_fr: Vec<(String, f64)> = vec![];
-
-    let start_timestamp = (Utc::now() - Duration::minutes(30)).timestamp_millis() as u64;
-    println!("{start_timestamp}");
-
-    for token in tokens {
-        println!("{token}");
-        let fhr = info_client
-            .funding_history(token.clone(), start_timestamp, None)
-            .await?;
-
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-
-        let fhr = fhr.last().unwrap();
-        println!("{:#?}", fhr);
-
-        let funding_rate = fhr.funding_rate.parse::<f64>().unwrap_or(0.0);
-
-        tokens_with_fr.push((fhr.coin.clone(), funding_rate));
-    }
-
-    Ok(tokens_with_fr)
-}
-
-async fn funding_history_example(info_client: &InfoClient) {
-    let coin = "MKR";
-
-    let start_timestamp = 1724803039095;
-    let end_timestamp = None;
-    info!(
-        "{:#?}",
-        info_client
-            .funding_history(coin.to_string(), start_timestamp, end_timestamp)
-            .await
-            .unwrap()
-    );
 }
